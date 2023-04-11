53d62a7ef056d998291dd36a8497b53d68a0b37b
HanyaAdel <hanya.elged@gmail.com>
2023-04-11 17:00:35 +0200

    outlier detection

---
+++ preprocessing.py
@@ -0,0 +1,23 @@
+
+import pandas as pd
+
+def drop_outliers_IQR(df):
+
+   q1=df.quantile(0.25)
+
+   q3=df.quantile(0.75)
+
+   IQR=q3-q1
+
+   df = df[~((df<(q1-1.5*IQR))|(df>(q3+1.5*IQR))).any(axis=1)]
+
+   return df
+
+
+
+#load the data into a dataframe
+
+df = pd.read_csv('heart.csv')
+
+df = drop_outliers_IQR(df)
+print (df.shape)
